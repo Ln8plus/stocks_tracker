94680b2aae9485925847a993f7d03e4ad09957d1
Ln8plus <71310400+Ln8plus@users.noreply.github.com>
2020-12-07 20:42:29 +0530

    Recents menu now shows app name

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Stocks Tracker',
       theme: ThemeData(
         scaffoldBackgroundColor: kBackgroundColor,
         textTheme: Theme.of(context).textTheme.apply(displayColor: kTextColor),
